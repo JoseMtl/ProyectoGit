5f1f24617c069fd67c2995c92e039c030ad8f248
JoseDespliegues <50029586+JoseDespligue@users.noreply.github.com>
2019-04-26 21:13:25 +0200

    Create NuevaClase.java

---
+++ src/proyectogit/NuevaClase.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectogit;
+
+/**
+ *
+ * @author Jose Gómez del Pino
+ */
+public class NuevaClase {
+    // Creamos una nueva clase con el usuario externo
+}
